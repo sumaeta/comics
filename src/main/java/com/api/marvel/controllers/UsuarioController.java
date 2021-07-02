ac50732aa89263ded25b0d3be7017f9f9387f75f
sumaeta <brenosumaeta33@gmail.com>
2021-07-02 13:36:45 -0300

    Fazendo o endpoint usuario retornar uma lista com os usuario cadastrados

--- src/main/java/com/api/marvel/controllers/UsuarioController.java
+++ src/main/java/com/api/marvel/controllers/UsuarioController.java
@@ -1,9 +1,25 @@
 package com.api.marvel.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.marvel.entities.Usuario;
+import com.api.marvel.repositories.UsuarioRepository;
 
 @RestController
 public class UsuarioController {
 
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@RequestMapping("/usuarios")
+	public ResponseEntity<List<Usuario>> lista(Usuario obj){
+		List<Usuario> lista = usuarioRepository.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
 	
 }
