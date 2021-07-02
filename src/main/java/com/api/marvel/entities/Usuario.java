918ab97ef246bedb790d61834cbb1a2a275fd79c
sumaeta <brenosumaeta33@gmail.com>
2021-07-02 12:59:58 -0300

    Criando a classe usuario e colocando o email e cpf como unico

---
+++ src/main/java/com/api/marvel/entities/Usuario.java
@@ -0,0 +1,77 @@
+package com.api.marvel.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column(unique = true)
+	private String cpf;
+	private String dataNascimento;
+	
+	public Usuario() {
+	}
+
+	public Usuario(Long id, String nome, String email, String cpf, String dataNascimento) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	 
+	
+}
