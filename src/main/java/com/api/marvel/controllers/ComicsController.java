01fd180dd887f797f4e60843dc536bfd6de91904
sumaeta <brenosumaeta33@gmail.com>
2021-07-02 14:20:47 -0300

    Criando o contralador da classe comics

--- src/main/java/com/api/marvel/controllers/ComicsController.java
+++ src/main/java/com/api/marvel/controllers/ComicsController.java
@@ -1,8 +1,24 @@
 package com.api.marvel.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.api.marvel.entities.Comics;
+import com.api.marvel.repositories.ComicsRepository;
 
 @RestController
 public class ComicsController {
 
+	@Autowired
+	private ComicsRepository comicsRepository;
+	
+	@RequestMapping("/comics")
+	public ResponseEntity<List<Comics>> lista(){
+		List<Comics> lista = comicsRepository.findAll();
+		return ResponseEntity.ok().body(lista);
+	}
 }
