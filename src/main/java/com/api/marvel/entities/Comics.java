231486db8be904a13b999a6f6e4916bfacaad603
sumaeta <brenosumaeta33@gmail.com>
2021-07-02 13:11:51 -0300

    Criando a classe comics

---
+++ src/main/java/com/api/marvel/entities/Comics.java
@@ -0,0 +1,123 @@
+package com.api.marvel.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Comics {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String comicsId;
+	private String titulo;
+	private Double preco;
+	private String autores;
+	private Integer isbn;
+	private String descricao;
+	
+	public Comics() {
+	}
+
+	public Comics(String comicsId, String titulo, Double preco, String autores, Integer isbn, String descricao) {
+		this.comicsId = comicsId;
+		this.titulo = titulo;
+		this.preco = preco;
+		this.autores = autores;
+		this.isbn = isbn;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getComicsId() {
+		return comicsId;
+	}
+
+	public void setComicsId(String comicsId) {
+		this.comicsId = comicsId;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public String getAutores() {
+		return autores;
+	}
+
+	public void setAutores(String autores) {
+		this.autores = autores;
+	}
+
+	public Integer getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((isbn == null) ? 0 : isbn.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Comics other = (Comics) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (isbn == null) {
+			if (other.isbn != null)
+				return false;
+		} else if (!isbn.equals(other.isbn))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
