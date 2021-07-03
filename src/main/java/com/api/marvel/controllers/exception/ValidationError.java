6ace7e7f61d5444fbabda59cbf9af432debe1921
sumaeta <brenosumaeta33@gmail.com>
2021-07-03 12:27:39 -0300

    Implementando o tratamento de excessões

---
+++ src/main/java/com/api/marvel/controllers/exception/ValidationError.java
@@ -0,0 +1,29 @@
+package com.api.marvel.controllers.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.api.marvel.services.exceptions.FieldMessage;
+
+public class ValidationError extends StandardError{
+
+	
+	private static final long serialVersionUID = 1L;
+
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+
+}
+	
